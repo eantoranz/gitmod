d0032ee299cf2957573677c8bec5a2df4b54aebc
Edmundo Carmona Antoranz <eantoranz@gmail.com>
2024-06-06 23:05:45 +0200

    scripts: consolidate deb scripts

--- scripts/deb.sh
+++ scripts/deb.sh
@@ -3,38 +3,59 @@
 # Copyright 2024 Edmundo Carmona Antoranz
 # Released under the terms of GPLv2
 
-# this script will be called from the root of the project that will be built.
 #
 # It will start an image to be able to build the project mounting the root of
 # the project in /mnt/work
 #
 # Parameters
+# - action (build, package, cli)
 # - distro (ubuntu, debian)
 # - docker tag (stable, testing, buster, 22.04, etc)
-# - git committish that will be used to build
+# - git committish that will be used to build (needed only when calling build)
 
 if [ $# -lt 3 ]; then
 	echo Not enough parameters. Need to provide:
+	echo - action "(build, package, cli)"
 	echo - distro "(ubuntu, debian)"
 	echo - distro docker tag "(stable, testing, buster, 22.04, etc)"
-	echo - git committish that will be used for building/packaging
+	echo - if the action is \"package\", the committish to package.
 	exit 1
 fi
 
-export DISTRO=$1
-export DOCKER_TAG=$2
-export COMMITTISH=$3
-export VERSION=$( git show $COMMITTISH:.gitmod_VERSION.txt 2> /dev/null | sed 's/.dirty//' )
+export ACTION=$1
+export DISTRO=$2
+export DOCKER_TAG=$3
 
-if [ "$VERSION" == "" ]; then
-	echo Version file "(.gitmod_VERSION.txt)" does not exit in $COMMITTISH or it is empty 1>&2
-	exit 1
-fi
-
+echo action: $ACTION
 echo distro: $DISTRO
 echo distro docker tag: $DOCKER_TAG
-echo git committish: $COMMITTISH
-echo gitmod version: $VERSION
+case $ACTION in
+build)
+	ACTOR=builder
+	;;
+cli)
+	ACTOR=cli
+	;;
+package)
+	if [ $# -lt 4 ]; then
+		echo You also need to specify which committish to package
+		exit 1
+	fi
+	export COMMITTISH=$4
+	export VERSION=$( git show $COMMITTISH:.gitmod_VERSION.txt 2> /dev/null | sed 's/.dirty//' )
+	if [ "$VERSION" == "" ]; then
+		echo Version file "(.gitmod_VERSION.txt)" does not exit in $COMMITTISH or it is empty. 1>&2
+		exit 1
+	fi
+	echo git committish: $COMMITTISH
+	echo git version: $VERSION
+	ACTOR=packager
+	;;
+*)
+	echo Unknown action $ACTION
+	exit 1
+esac
+
 
 DOCKER_IMAGE="gitmod-debbuilder-$DISTRO-$DOCKER_TAG"
 
@@ -49,5 +70,5 @@ docker run --rm -ti -v "$PWD:/mnt/work" -w /mnt/work \
 	-e DOCKER_TAG=$DOCKER_TAG \
 	-e VERSION=$VERSION \
 	-e COMMITTISH=$COMMITTISH \
-	--name gitmod-debpackager-$DISTRO-$DOCKER_TAG \
-	$DOCKER_IMAGE scripts/deb/packager.sh
+	--name gitmod-deb$ACTOR-$DISTRO-$DOCKER_TAG \
+	$DOCKER_IMAGE scripts/deb/$ACTION.sh
